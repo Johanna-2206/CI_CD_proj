8f750036507d029ad1fc00e4e74614ee9f0a8498
Johanna-2206 <126691370+Johanna-2206@users.noreply.github.com>
2023-03-02 15:38:21 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ function myfunc() {
     b7=document.getElementById("b7").value;
     b8=document.getElementById("b8").value;
     b9=document.getElementById("b9").value;
-}
-if(b1.toLowerCase()=='x'&& b2.toLowerCase()=='x'&& b3.toLowerCase()=='x'){
+
+if (b1.toLowerCase()=='x'&& b2.toLowerCase()=='x'&& b3.toLowerCase()=='x'){
     window.alert('You are a winner');
     reset();
-} else if(b4.toLowerCase()=='x'&& b5.toLowerCase()=='x'&& b6.toLowerCase()=='x'){
+}else if(b4.toLowerCase()=='x'&& b5.toLowerCase()=='x'&& b6.toLowerCase()=='x'){
     window.alert('You are a winner');
     reset();
 }else if(b7.toLowerCase()=='x'&& b8.toLowerCase()=='x'&& b9.toLowerCase()=='x'){
@@ -35,10 +35,10 @@ if(b1.toLowerCase()=='x'&& b2.toLowerCase()=='x'&& b3.toLowerCase()=='x'){
     window.alert('You are a winner');
     reset();
 }
-if(b1.toLowerCase()=='o'&& b2.toLowerCase()=='o'&& b3.toLowerCase()=='o'){
+if (b1.toLowerCase()=='o'&& b2.toLowerCase()=='o'&& b3.toLowerCase()=='o'){
     window.alert('You are a winner');
     reset();
-} else if(b4.toLowerCase()=='o'&& b5.toLowerCase()=='o'&& b6.toLowerCase()=='o'){
+}else if(b4.toLowerCase()=='o'&& b5.toLowerCase()=='o'&& b6.toLowerCase()=='o'){
     window.alert('You are a winner');
     reset();
 }else if(b7.toLowerCase()=='o'&& b8.toLowerCase()=='o'&& b9.toLowerCase()=='o'){
@@ -59,6 +59,7 @@ if(b1.toLowerCase()=='o'&& b2.toLowerCase()=='o'&& b3.toLowerCase()=='o'){
 }else if(b3.toLowerCase()=='o'&& b5.toLowerCase()=='o'&& b7.toLowerCase()=='o'){
     window.alert('You are a winner');
     reset();
+}
 }
 function reset(){
     location.reload();
